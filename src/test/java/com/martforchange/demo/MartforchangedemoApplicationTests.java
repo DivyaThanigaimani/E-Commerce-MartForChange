f21dc7ca65e94dbc5d2f22f68b9ce787fd7ae0b6
DivyaThanigaimani <thanigad@uwindsor.ca>
2023-03-22 16:30:29 -0400

    project chnages

---
+++ src/test/java/com/martforchange/demo/MartforchangedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martforchange.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartforchangedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

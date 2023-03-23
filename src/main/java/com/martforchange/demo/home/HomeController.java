01509f553f1908b5981c36c1a05348bd43cdd62f
DivyaThanigaimani <thanigad@uwindsor.ca>
2023-03-22 21:35:36 -0400

    chnge in url

--- src/main/java/com/martforchange/demo/home/HomeController.java
+++ src/main/java/com/martforchange/demo/home/HomeController.java
@@ -2,13 +2,15 @@ package com.martforchange.demo.home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@RequestMapping("/")
 public class HomeController {
 
-	@GetMapping("/")
+	@GetMapping("home")
     public String index() {
-        return "index";
+        return "orderassigned";
     }
 }
